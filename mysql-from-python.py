a4d267a933a5d6b6f6c2e75a34d3d7539da1d57e
Rick N <rickmnassar@gmail.com>
2020-12-31 13:06:31 +0000

    Learning how to Delete Where In

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,8 +11,14 @@ connection = pymysql.connect(host='localhost',
                              password='',
                              db='Chinook')
 try:
-     with connection.cursor() as cursor:
-        rows = cursor.execute("DELETE FROM Friends WHERE name = 'bob';")
+    with connection.cursor() as cursor:
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+
         connection.commit()
 finally:
     connection.close()
